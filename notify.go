4877723296c1487f6805e61f57cf54d6dbef189a
dev <dev@qiyutech.tech>
2021-06-26 21:34:45 +0800

    works

---
+++ notify.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "github.com/QiYuTechDev/qiyu-notify-go/cli"
+    "os"
+)
+
+func main() {
+    if err := cli.CliCmd.Execute(); err != nil {
+        _, _ = fmt.Fprintln(os.Stderr, err)
+        os.Exit(1)
+    }
+}
