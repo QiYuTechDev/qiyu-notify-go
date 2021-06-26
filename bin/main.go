c55ae424814417c10b015d8319a9cefeea00aad8
dev <dev@qiyutech.tech>
2021-06-26 21:30:46 +0800

    add for cli

--- bin/main.go
+++ bin/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/QiYuTechDev/qiyu-notify-go/cli"
 	"os"
 )
 
 func main() {
-	if err := cliCmd.Execute(); err != nil {
+	if err := cli.CliCmd.Execute(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
--- cli/cli.go
+++ cli/cli.go
@@ -1,10 +1,10 @@
-package main
+package cli
 
 import (
 	"github.com/spf13/cobra"
 )
 
-var cliCmd = &cobra.Command{
+var CliCmd = &cobra.Command{
 	Short: "notify 奇遇推送命令行工具",
 	Long: `notify 是一个在任意地方给您发送通知的命令行工具
 
