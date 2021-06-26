950158a19e7d30ba25e1749d3ccd73393d5989b4
dev <dev@qiyutech.tech>
2021-06-26 18:09:12 +0800

    add build query

--- api/base.go
+++ api/base.go
@@ -15,17 +15,26 @@ func sendHttpRequest(r *http.Request) (io.ReadCloser, error) {
 	return resp.Body, nil
 }
 
-func buildUrl(baseUrl, pathUrl string, params map[string]string) (*url.URL, error) {
+func buildUrl(baseUrl, pathUrl string, pathArgs, queryArgs map[string]string) (*url.URL, error) {
 	parts := strings.Split(pathUrl, "/")
 	out := make([]string, len(parts))
 	for idx, p := range parts {
 		if strings.HasPrefix(p, "{") && strings.HasSuffix(p, "}") {
-			out[idx] = params[p[1:len(p)-1]]
+			out[idx] = pathArgs[p[1:len(p)-1]]
 		} else {
 			out[idx] = p
 		}
 	}
 	realPath := strings.Join(out, "/")
 	fullUrl := baseUrl + realPath
-	return url.Parse(fullUrl)
+	u, err := url.Parse(fullUrl)
+	if err != nil {
+		return nil, err
+	}
+	q := u.Query()
+	for key, value := range queryArgs {
+		q.Set(key, value)
+	}
+	u.RawQuery = q.Encode()
+	return u, nil
 }
